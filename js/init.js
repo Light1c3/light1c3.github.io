309901ee7d120ad4e6ca9e6b69f7a131c4e354cb
Light1c3 <fullmetala@walla.com>
2016-10-30 13:01:59 -0400

    testing out a new smoothscroll function

--- js/init.js
+++ js/init.js
@@ -25,9 +25,11 @@
 	    var target = this.hash,
 	    $target = $(target);
 
-      $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-      }, 800);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
 	});
 
 
